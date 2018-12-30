e8328f3ad00787f692586d72a3077c11c9373ca0
Xiao Wang(Hsiao Wong) <pangtouyu94@gmail.com>
2018-12-30 14:32:33 -0800

    Update 200NumberOfIslands.java

--- 200NumberOfIslands.java
+++ 200NumberOfIslands.java
@@ -1,40 +1,34 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int num = 0;
-        for(int row =0;row < grid.length;row ++){
-            for (int column=0; column < grid[row].length;column++){
-                num += getnumber(grid, row, column);
+        if(grid == null || grid.length == 0){
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        int res = 0;
+        for(int i =0 ; i< row; i++){
+            for(int j =0; j< col; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid,i,j);
+                    res++;
+                }
+                
             }
             
         }
-        
-        return num;
-        
-    }
-    
-    public int getnumber(char[][] grid, int row, int column){
-        /*if(row <0 || column < 0 || row >= grid.length || column >= grid[row].length){
-            return 0;
-        }*/
-        if(grid[row][column] == '0') {return 0;}
-        clearIs(grid,row,column);
-            
-        return 1;    
-        
+        return res;
         
     }
     
-    public void clearIs(char[][] grid, int row, int column){
-        if(row <0 || column < 0 || row >= grid.length || column >= grid[row].length){
+    public void dfs(char[][] grid, int i, int j){
+        if(i< 0|| j<0 || i>= grid.length|| j>= grid[0].length || grid[i][j] == '0'){
             return;
         }
-        if(grid[row][column] == '0') {return;}
-        grid[row][column] = '0';
-        int[] dx = {0,0,-1,1};
-        int[] dy = {-1,1,0,0};
-        for (int k =0; k< 4;k++){
-            clearIs(grid, row + dx[k], column + dy[k]);
-        }
         
+        grid[i][j] = '0';
+        dfs(grid, i+1,j);
+        dfs(grid, i-1,j);
+        dfs(grid, i,j+1);
+        dfs(grid, i,j-1);
     }
 }
